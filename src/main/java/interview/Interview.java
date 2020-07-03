2a931b0bc8e9ea92c4a97979e8f30ad493c9de3d
yqx254 <ravix254@outlook.com>
2020-07-03 14:49:16 +0800

    Solve interview 02 03 04 05

--- src/main/java/interview/Interview.java
+++ src/main/java/interview/Interview.java
@@ -24,4 +24,123 @@ public class Interview {
         }
         return true;
     }
+
+    /**
+     *  01.02 判定是否互为字符重排
+     *  给定两个字符串 s1 和 s2
+     *  请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+     * @param s1 字符串1
+     * @param s2 字符串2
+     * @return 判断结果
+     * 思路：先想到map，效率不高
+     * 使用整型数组来记录字符出现频率，代替map
+     * 长度固定、内容为基本型时，使用数组而非集合，这也是集合的最佳实践之一
+     */
+    public boolean checkPermutation(String s1, String s2) {
+        int [] res = new int[128];
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        for(int i = 0; i < s1.length(); i ++){
+            res[s1.charAt(i) ] ++;
+            res[s2.charAt(i) ] --;
+        }
+        for(int i : res){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 01.03. URL化
+     * URL化。编写一种方法，将字符串中的空格全部替换为%20。
+     * 假定该字符串尾部有足够的空间存放新增字符，并且知道字符串的“真实”长度。
+     * @param s 字符串
+     * @param length 真实长度
+     * @return
+     * 题意难懂，抄来的代码在不同环境执行会出现问题
+     * 垃圾题目，不解释
+     */
+    public String replaceSpaces(String s, int length){
+        int i = length - 1;
+        int j = s.length() - 1;
+        char [] res = s.toCharArray();
+        while(i >= 0){
+            if(res[i] == ' '){
+                res[j--] = '0';
+                res[j--] = '2';
+                res[j--] = '%';
+            }
+            else{
+                res[j--]  = res[i];
+            }
+            i --;
+        }
+        return String.valueOf(res,j + 1,s.length() - j - 1);
+    }
+
+    /**
+     *  01.04. 回文排列
+     *  给定一个字符串，编写一个函数判定其是否为某个回文串的排列之一。
+     * @param s 给定字符串
+     * @return 是否是回文串
+     * 思路： 本质就是int数组（替代map）记录字符出现次数，用于判断回文
+     * 出现一次+1 出现两次-1，最终int数组的和只能是0和1，才是一个有效的回文串
+     */
+    public boolean canPermutePalindrome(String s){
+        int [] record = new int [128];
+        for(char c : s.toCharArray()){
+            if(record[c] == 1){
+                record[c]--;
+            }
+            else{
+                record[c] ++;
+            }
+        }
+        int count = 0;
+        for(int a : record){
+            if(count > 1){
+                return false;
+            }
+            count += a;
+        }
+        return true;
+    }
+
+    /**
+     *   01.05. 一次编辑
+     *   字符串有三种编辑操作:插入一个字符、删除一个字符或者替换一个字符。
+     *   给定两个字符串，编写一个函数判定它们是否只需要一次(或者零次)编辑。
+
+     * @param first 第一个字符串
+     * @param second 第二个字符串
+     * @return 一次编辑是否可达
+     * 思路：起手进坑：使用上一题类似的思路进行记录和判断
+     * 但没有考虑到顺序问题（见测试用例3）
+     * 正儿八经的思路：把长的字符串调整为first，挨个比较
+     * 若发现不通值的字符a[i]，则检查f.substring(i +1)  s.substring(i +1)
+     * 或者f.substring(i + 1)  s.substring(i)是否相等，若相等则一步可达
+     */
+    public boolean oneEditAway(String first, String second){
+        if(Math.abs(first.length() - second.length()) >1){
+            return false;
+        }
+        if(first.length() < second.length()){
+            String tmp = second;
+            second = first;
+            first = tmp;
+        }
+        for(int i = 0; i < second.length(); i ++){
+            if(first.charAt(i) != second.charAt(i)){
+                if(first.substring(i + 1).equals(second.substring(i + 1)) ||
+                    first.substring(i + 1).equals(second.substring(i))){
+                    return true;
+                }
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/interview/InterviewTest.java
+++ src/test/interview/InterviewTest.java
@@ -22,4 +22,61 @@ public class InterviewTest {
         assertTrue(interview.isUnique(s2));
         assertTrue(interview.isUnique(s3));
     }
+
+    @Test
+    public void test_check_permutation(){
+        String s11 = "abcc";
+        String s12 = "cbca";
+
+        String s21 = "terror";
+        String s22 = "teiror";
+
+        String s31 = "goody";
+        String s32 = "dooyg";
+
+        String s41 = "";
+        String s42 = "";
+
+        String s51 = "Account";
+        String s52 = "aCcount";
+
+        assertTrue(interview.checkPermutation(s11, s12));
+        assertFalse(interview.checkPermutation(s21, s22));
+        assertTrue(interview.checkPermutation(s31, s32));
+        assertTrue(interview.checkPermutation(s41, s42));
+        assertFalse(interview.checkPermutation(s51, s52));
+    }
+
+
+    @Test
+    public void test_can_permutePalindrome(){
+        String s1 = "tactcoa";
+        String s2 = "";
+        String s3 = "some";
+        String s4 = "naa";
+        assertTrue(interview.canPermutePalindrome(s1));
+        assertTrue(interview.canPermutePalindrome(s2));
+        assertFalse(interview.canPermutePalindrome(s3));
+        assertTrue(interview.canPermutePalindrome(s4));
+    }
+
+    @Test
+    public void test_one_edit_away(){
+        String sf1 = "pale";
+        String ss1 = "ple";
+
+        String sf2 = "pales";
+        String ss2 = "pal";
+
+        String sf3 = "something";
+        String ss3 = "somaehing";
+
+        String sf4 = "a";
+        String ss4 = "ea";
+
+        assertTrue(interview.oneEditAway(sf1, ss1));
+        assertFalse(interview.oneEditAway(sf2, ss2));
+        assertFalse(interview.oneEditAway(sf3, ss3));
+        assertTrue(interview.oneEditAway(sf4, ss4));
+    }
 }
