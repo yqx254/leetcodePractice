79d5c2f3d4b39a4692209dd4f058319e950b4602
yqx254 <ravix254@outlook.com>
2020-07-21 16:45:30 +0800

    17.06 in progress

--- src/main/java/interview/Interview.java
+++ src/main/java/interview/Interview.java
@@ -980,4 +980,51 @@ public class Interview {
     private boolean checkValid(int r, int c){
         return row[r] + col[c] + vale[r - c + len] + dale[r + c] == 0;
     }
+
+    /**
+     * 16.10. 生存人数
+     * 给定N个人的出生年份和死亡年份，第i个人的出生年份为birth[i]，死亡年份为death[i]
+     * 实现一个方法以计算生存人数最多的年份。
+     * 你可以假设所有人都出生于1900年至2000年（含1900和2000）之间。
+     * 如果一个人在某一年的任意时期都处于生存状态，那么他们应该被纳入那一年的统计中。
+     * 例如，生于1908年、死于1909年的人应当被列入1908年和1909年的计数。
+     * 如果有多个年份生存人数相同且均为最大值，输出其中最小的年份。
+     * @param birth 出生年份
+     * @param death 死亡年份
+     * @return 存活人数最多的年份
+     * 思路：使用数量变化数组存储每一年生死人数的变化，再加一套循环寻找最大值
+     * 比较有复习意义的题目
+     */
+    public int maxAliveYear(int []birth, int []death){
+        int len = birth.length;
+        int [] change = new int[120];
+        int max = 1900;
+        int countMax = 0;
+        for(int i = 0; i < len; i ++){
+            change[birth[i] - 1900] ++;
+            change[death[i] - 1899] -- ;
+        }
+        int count = 0;
+        for(int j = 0; j < change.length; j ++){
+            count += change[j];
+            if(count > countMax){
+                countMax = count;
+                max = 1900 + j;
+            }
+        }
+        return max;
+    }
+
+    /**
+     *  17.06. 2出现的次数
+     *  编写一个方法，计算从 0 到 n (含 n) 中数字 2 出现的次数。
+     *  暂时总结规律如下
+     *n=10	i = 0	1
+     *
+     * n=100	i = 1	val(i - 1) * 10 + n % 10  = 20
+     *
+     * n = 1000	i = 2         val(i - 1) * 10 + n % 10  = 300
+     *
+     * n = 10000  i = 3      val(i - 1) * 10 + n % 10 = 4000
+     */
 }
