46b29e46b8b237260ca247c4bc8107624735c442
yqx254 <ravix254@outlook.com>
2020-07-29 21:19:45 +0800

    No.240 reviewed

--- src/main/java/medium/Medium.java
+++ src/main/java/medium/Medium.java
@@ -1314,6 +1314,17 @@ public class Medium {
         return true;
     }
 
+    /**
+     * 240搜索二维矩阵 II
+     * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+     * 每行的元素从左到右升序排列。
+     * 每列的元素从上到下升序排列。
+     * @param matrix 矩阵
+     * @param target 目标数
+     * @return 是否存在
+     * 思路：分析矩阵图可知，这个矩阵类似一个从左下方开始的查找二叉树，故能得解
+     * （这个分析并不容易）
+     */
     public boolean searchMatrix(int[][] matrix, int target) {
         int i = matrix.length - 1;
         int j = 0;
