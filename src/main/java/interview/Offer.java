b10ecb5cd13416ac2eb7d714dd30e43599675210
yqx254 <ravix254@outlook.com>
2020-07-31 21:49:25 +0800

    Solve offer 25

--- src/main/java/interview/Offer.java
+++ src/main/java/interview/Offer.java
@@ -228,4 +228,30 @@ public class Offer {
         }
         return  head;
     }
+
+    /**
+     * Offer 25. 合并两个排序的链表
+     * @param l1 链表1
+     * @param l2 链表2
+     * @return 合并后的链表
+     * 思路： 常规操作，骚套路不太好想
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode head = new ListNode(0);
+        ListNode tmp = head;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                tmp.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                tmp.next = l2;
+                l2 = l2.next;
+            }
+            tmp = tmp.next;
+        }
+        tmp.next = l1 != null ? l1 : l2;
+        return head.next;
+    }
+
 }
